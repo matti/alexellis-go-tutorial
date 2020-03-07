ae1b6c1e4e50419ebb4b0cdabfeddaa8e411be04
Matti Paksula <matti.paksula@iki.fi>
2020-03-07 12:37:19 +0200

    :sunrise:

---
+++ cli/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var printUser bool
+
+	flag.BoolVar(&printUser, "print-user", true, "Print a name if set to true")
+	flag.Parse()
+
+	user := os.Getenv("USER")
+
+	if printUser {
+		fmt.Println("Hello " + user)
+	}
+}
